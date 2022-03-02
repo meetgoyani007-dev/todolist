36594fbc2be150f49d3e57e90592034b656d0c0a
Meet Goyani <goyanimeet007@gmail.com>
2022-03-02 22:18:04 +0530

    resolve task create issue

--- app/controllers/tasks.controller.js
+++ app/controllers/tasks.controller.js
@@ -9,9 +9,9 @@ exports.create = (req, res) => {
   if (!req.body["description"] || !req.body["status"]) {
     res.status(400).send({
       message: `task can not be empty!${
-        (!req.body["description"] || !req.body["status"],
+        (!req.body["description"] || !req.body["status"]),
         !req.body["description"],
-        !req.body["status"])
+        !req.body["status"]
       }`,
     });
     return;
